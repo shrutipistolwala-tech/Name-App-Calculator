1dcc0449e5b9e87fc60fa1e1b3212905a53f63c5
Shruti Pistolwala <67134172+shrutipistolwala-tech@users.noreply.github.com>
2021-01-03 23:43:40 +0100

    Name App Calculator

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
 (function() {
     'use strict';
 
-  angular.module('myApp',[])  
+    angular.module('NameCalculator', [])
 
-  .controller('MyFirstController', function ($scope){
-     $scope.name = "Shruti";
-    //  $scope.sayHello =  function (){
-    //      return "Hello";
-    //  }; 
+    .controller('NameCalculatorController', function ($scope) {
+      $scope.name = "";
+      $scope.totalValue = 0;
+      $scope.displayNumeric = function () {
+        var totalNameValue = calculateNumericForString($scope.name); 
+        $scope.totalValue = totalNameValue;
+      };
 
-  });
-  
+      function calculateNumericForString(string) {
+        var totalStringValue = 0;
+        for(var i = 0; i<string.length; i++) {
+          totalStringValue += string.charCodeAt(i);
+        }
+        return totalStringValue;
+      }
 
+    });
 
-})();
+  })();
